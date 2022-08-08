729d738dcdf909986d5fa8877e210b8cfb783aff
Muhammad Saqib Khan <91668456+saqib-niaxi@users.noreply.github.com>
2022-08-08 13:38:40 +0500

    updated solved problems

--- class6practise.py
+++ class6practise.py
@@ -1,4 +1,23 @@
-# write a program to print first 10 even numbers in reverse order
-for i in range(10):
-    print(20 - 2 * i)
-    
+# Write a program that accepts a sentence and calculate the number of letters and digits.
+
+# Suppose the following input is supplied to the program:
+
+# hello world! 123
+
+# Then, the output should be:
+
+# LETTERS 10
+
+# DIGITS 3
+inp = input("Enter a sentence: ")
+letters = 0
+digits = 0
+for i in inp:
+    if i.isalpha():
+        letters += 1
+    elif i.isdigit():
+        digits += 1
+    else:
+        pass
+print("LETTERS", letters)
+print("DIGITS", digits)
